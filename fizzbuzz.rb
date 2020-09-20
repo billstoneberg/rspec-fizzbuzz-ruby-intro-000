89a86a57c9e0b43b093f14a1af77f15e300e1e99
William Stoneberg <wrstoneberg@me.com>
2020-09-20 22:46:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,7 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number / 3
+    return "Fizz"
+  else
+  end
+end# Don't forget! This file needs to be 'required' in its spec file
+# See R EADME.md for instructions on how to do this
