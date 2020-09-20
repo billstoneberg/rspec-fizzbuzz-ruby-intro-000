c2c2ec9f182944f0fe185a808433a1513620b460
William Stoneberg <wrstoneberg@me.com>
2020-09-20 22:50:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,9 @@ require 'pry'
 def fizzbuzz(number)
   if number / 3 == .i
     return "Fizz"
-    binding.pry
+
   else
   end
+  binding.pry
 end# Don't forget! This file needs to be 'required' in its spec file
 # See R EADME.md for instructions on how to do this
